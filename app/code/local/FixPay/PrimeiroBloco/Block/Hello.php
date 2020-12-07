48539177857de85303a0bb7a99201942f84ee5cd
Pedro Henrique S B <pedrosaints@alu.ufc.br>
2020-12-07 16:13:42 -0300

    configs

--- app/code/local/FixPay/PrimeiroBloco/Block/Hello.php
+++ app/code/local/FixPay/PrimeiroBloco/Block/Hello.php
@@ -4,4 +4,12 @@ class FixPay_PrimeiroBloco_Block_Hello extends Mage_Core_Block_Template {
     public function getHello(){
         echo "Hello World";
     }
+
+    public function getEmail(){
+        echo Mage::getStoreConfig("fixpay_configs/fixpay_credenciais/fixpay_email");
+    }
+
+    public function getSenha(){
+        echo Mage::getStoreConfig("fixpay_configs/fixpay_credenciais/fixpay_senha");
+    }
 }
